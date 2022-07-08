d33d41f5dadfc18a26ba2fc3141805fba7ff5bdd
jimmy <joaquin.fox@gmail.com>
2022-07-07 20:52:27 -0700

    updates

---
+++ index.js
@@ -0,0 +1,9 @@
+const targetBlock = Document.querySelector('ul');
+
+function hoverEffect() {
+  //   const thisInstance = document.querySelector(this);
+  //   return "hanga banga";
+  console.log("hanga banga");
+}
+
+console.log(hoverEffect());
